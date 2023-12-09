8ac31804e8e485d8d7556abdcd13ec07b43bc008
Samcoding5854 <samcoding5854@gmail.com>
2023-12-09 14:09:26 +0530

    Edited the Registration page

--- Frontend/src/components/Registration.js
+++ Frontend/src/components/Registration.js
@@ -1,62 +1,110 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
+import { ref, uploadBytes } from 'firebase/storage';
+import { v4 } from 'uuid';
+import { img_viddb } from '../config';
 
-export default function Registration () { 
-   const [user,setuser]=useState({
-    upld:"",
-    name:"",
-    licence:"",
+export default function Registration() {
+  const [user, setUser] = useState({
+    upld: '',
+    name: '',
+    licence: '',
+  });
 
-   });
-let nme,value;
-   const getuserData=(e) =>{
-    nme= e.target.name;
-    value= e.target.value;
+  const [selected, setSelect] = useState(null);
 
-    setuser({...user,[nme]:value});
-   };
-   const postdata=async(e) =>{
-      e.preventDefault();
+  const getuserData = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
 
-      const {upld,name,licence} = user;
-      
-      if(upld && name && licence){
-        const res=await fetch("https://numplate-face-default-rtdb.firebaseio.com/num-face.json",{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json",
+  const postdata = async (e) => {
+    e.preventDefault();
+    const { upld, name, licence } = user;
+
+    if (name && licence) {
+      const res = await fetch('https://numplate-face-default-rtdb.firebaseio.com/num-face.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-        body:JSON.stringify( {upld,name,licence}),
-      } );
-      if(res){
-        setuser({upld:"",
-        name:"",
-        licence:"",});
-      }
-   }
-      else{
-        alert("please fill the form")
-      }
+        body: JSON.stringify({ name, licence }),
+      });
 
+      if (res) {
+        setUser({
+          upld: '',
+          name: '',
+          licence: '',
+        });
+        window.location.reload();
+      }
+    } else {
+      alert('Please fill the form');
     }
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+
+    if (!selected) {
+      alert('Please select a file');
+      return;
+    }
+
+    const img_reg = ref(img_viddb, `Registration/${v4()}`);
+    try {
+
+      const ress=await uploadBytes(img_reg, selected);
+      if(ress){
+      alert("photo uploded successfully")
+      setSelect(null);
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
   return (
     <div>
-      <form className='Registration'>
-      <div className='header'>
-        <h1>Registration</h1>
-      </div>
-      <form className='regitration-form' method='POST'>
-      <div className='register'>
-        <input type='file' name='upld' onChange={getuserData} value={user.upld}  className='upload' required></input>
-        <br></br>
-        <input type='text' name='name' onChange={getuserData} value={user.name} placeholder='Enter your Name' className='name' required></input>
-        <br></br>
-        <input type='text' name='licence' onChange={getuserData} value={user.licence} placeholder='Enter your Vehicle Licence Number' className='lincence' required></input>
-      </div>
+      <form className="Registration" onSubmit={postdata}>
+        <div className="header">
+          <h1>Registration</h1>
+        </div>
+        <div className="register">
+          <input
+            type="file"
+            name="upld"
+            onChange={(e) => setSelect(e.target.files[0])}
+            className="upload"
+            required
+          />
+          <button type="button" onClick={handleUpload}>
+            Upload
+          </button>
+          <br />
+          <input
+            type="text"
+            name="name"
+            onChange={getuserData}
+            value={user.name}
+            placeholder="Enter your Name"
+            className="name"
+            required
+          />
+          <br />
+          <input
+            type="text"
+            name="licence"
+            onChange={getuserData}
+            value={user.licence}
+            placeholder="Enter your Vehicle Licence Number"
+            className="licence"
+            required
+          />
+        </div>
+        <br />
+        <button type="submit">Submit</button>
       </form>
-      <br></br>
-      <button onClick={postdata}>submit</button>
-    </form>
     </div>
-  )
-
+  );
 }
